d3bd4265e9f8d143956c615bdcdcfbc27bf148f5
Tomek Sulkowski <tomek.sulkowski@gmail.com>
2019-01-22 17:00:19 +0100

    simple searching + local template variable

--- src/app/tv/search/search.component.ts
+++ src/app/tv/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TvMazeService } from '../tv-maze.service';
+import { Show } from '../tv.models';
 
 @Component({
   selector: 'tm-search',
@@ -7,13 +8,18 @@ import { TvMazeService } from '../tv-maze.service';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+  shows: Show[] = [];
 
   constructor(private tv: TvMazeService) {
     this.tv.searchShows('flash')
-      .subscribe();
+      .subscribe(shows => this.shows = shows);
   }
 
   ngOnInit() {
   }
 
+  search(query: string) {
+    this.tv.searchShows(query)
+      .subscribe(shows => this.shows = shows);
+  }
 }
