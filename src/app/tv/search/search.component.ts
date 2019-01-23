18a44d4bfe7aabed0e23bf0da7b5e72336ed7409
Tomek Sulkowski <tomek.sulkowski@gmail.com>
2019-01-23 13:56:39 +0100

    adding and displaying bookmarks

--- src/app/tv/search/search.component.ts
+++ src/app/tv/search/search.component.ts
@@ -1,25 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { TvMazeService } from '../tv-maze.service';
 import { Show } from '../tv.models';
+import { BookmarksService } from '../../bookmarks/bookmarks.service';
 
 @Component({
   selector: 'tm-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
   shows: Show[] = [];
   query = 'flash';
 
-  constructor(private tv: TvMazeService) {
+  constructor(private tv: TvMazeService,
+              private bs: BookmarksService) {
     this.search(this.query);
-  }
-
-  ngOnInit() {
   }
 
   search(query: string) {
     this.tv.searchShows(query)
       .subscribe(shows => this.shows = shows);
+  }
+
+  saveBookmark(show: Show) {
+    this.bs.add(show);
+  }
+
+  isBookmarked({ id }: Show): boolean {
+    return this.bs.has(id);
+  }
+
+  get bookmarks(): Show[] {
+    return this.bs.getAll() as Show[];
   }
 }
