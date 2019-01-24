3911127082bbc162b4b1b60c31e0a32c354c986b
Tomek Sulkowski <tomek.sulkowski@gmail.com>
2019-01-24 14:49:07 +0100

    persisting bookmarks to the json-server backend

--- src/app/bookmarks/bookmarks.service.ts
+++ src/app/bookmarks/bookmarks.service.ts
@@ -1,22 +1,31 @@
 import { Injectable } from '@angular/core';
 import { Bookmark, BookmarkId } from './bookmarks.models';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookmarksService<T extends Bookmark = Bookmark> {
   private items: T[] = [];
+  private readonly apiRoot = 'http://localhost:3000/bookmarks';
+
+  constructor(private http: HttpClient) {
+    this.http.get<T[]>(this.apiRoot)
+      .subscribe(items => this.items = items);
+  }
 
   getAll(): T[] {
     return [...this.items];
   }
 
   add(item: T): void {
-    this.items.push(item);
+    this.http.post(this.apiRoot, item)
+      .subscribe(() => this.items.push(item));
   }
 
   remove(id: BookmarkId): void {
-    this.items = this.items.filter(item => item.id !== id);
+    this.http.delete(`${this.apiRoot}/${id}`)
+      .subscribe(() => this.items = this.items.filter(item => item.id !== id));
   }
 
   has(id: BookmarkId): boolean {
